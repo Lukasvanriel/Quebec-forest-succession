bb9bf226e04379f30775dda3b9f9fcdac070397a
Jane Doe <jane@example.org>
2023-08-02 17:32:37 -0400

    Added groups

--- R-scripts/01-Data/Load-SIFORT.R
+++ R-scripts/01-Data/Load-SIFORT.R
@@ -76,3 +76,24 @@ write.csv(bte2_noP, "../../../Data/BTE/bte2_noP.csv")
 write.csv(bte3_noP, "../../../Data/BTE/bte3_noP.csv")
 write.csv(bte4_noP, "../../../Data/BTE/bte4_noP.csv")
 write.csv(bte5_noP, "../../../Data/BTE/bte5_noP.csv")
+
+### Combine all data in one dataframe
+# Only keep columns that are present in bte5
+bte1_noP <- bte1_noP %>%
+  select(intersect(colnames(bte1_noP) , colnames(bte5_noP)))
+bte2_noP <- bte2_noP %>%
+  select(intersect(colnames(bte2_noP) , colnames(bte5_noP)))
+bte3_noP <- bte3_noP %>%
+  select(intersect(colnames(bte3_noP) , colnames(bte5_noP)))
+bte4_noP <- bte4_noP %>%
+  select(intersect(colnames(bte4_noP) , colnames(bte5_noP)))
+
+# Combine all
+bte_all <- rbind(bte1_noP, bte2_noP, bte3_noP, bte4_noP, bte5_noP)
+
+###There is a significant fraction of TESSELLE that are lacking Species information
+#TODO: Look into which to drop and which to keep
+bte <- bte_all |> filter(! is.na(GR_ESS))
+
+#### Write out combined data ####
+write.csv(bte_all, "../../../Data/BTE/bte_all.csv")
--- R-scripts/02-Define_classes/Define-classes.R
+++ R-scripts/02-Define_classes/Define-classes.R
@@ -1,29 +1,15 @@
 #### Script to group species classes ####
 
+#### Load data ####
 bte1_noP <- read.csv("../../../Data/BTE/bte1_noP.csv")[,-1]
 bte2_noP <- read.csv("../../../Data/BTE/bte2_noP.csv")[,-1]
 bte3_noP <- read.csv("../../../Data/BTE/bte3_noP.csv")[,-1]
 bte4_noP <- read.csv("../../../Data/BTE/bte4_noP.csv")[,-1]
 bte5_noP <- read.csv("../../../Data/BTE/bte5_noP.csv")[,-1]
 
-### Only keep columns that are present in bte5 ###
-bte1_noP <- bte1_noP %>%
-  select(intersect(colnames(bte1_noP) , colnames(bte5_noP)))
-bte2_noP <- bte2_noP %>%
-  select(intersect(colnames(bte2_noP) , colnames(bte5_noP)))
-bte3_noP <- bte3_noP %>%
-  select(intersect(colnames(bte3_noP) , colnames(bte5_noP)))
-bte4_noP <- bte4_noP %>%
-  select(intersect(colnames(bte4_noP) , colnames(bte5_noP)))
+bte <- read.csv("../../../Data/BTE/bte_all.csv")[,-1]
 
-### Combine data ####
-bte_all <- rbind(bte1_noP, bte2_noP, bte3_noP, bte4_noP, bte5_noP)
-
-###There is a significant fraction of TESSELLE that are lacking Species information
-#TODO: Look into which to drop and which to keep
-bte <- bte_all |> filter(! is.na(GR_ESS))
-
-###Extract the most prevalent classes
+#### Extract the most prevalent classes ####
 most_prev <- bte |> 
   count(GR_ESS) |> arrange(desc(n)) |> head(100)
 
@@ -31,4 +17,8 @@ most_prev <- bte |>
 bte <- bte |>
   mutate(dom_sp = substr(GR_ESS,1, 2))
 
-table(bte$dom_sp)
+most_prev_dom <- bte |> 
+  count(dom_sp) |> arrange(desc(n)) |> head(100)
+
+#### Divide into classes as defined by Grouping.R ####
+
