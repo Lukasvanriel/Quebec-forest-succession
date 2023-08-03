007d0fb7bc54e1a5dacf9566835d4527af995ee9
Jane Doe <jane@example.org>
2023-08-03 10:01:47 -0400

    Added Data

--- R-scripts/03-Analysis/Msm.R
+++ R-scripts/03-Analysis/Msm.R
@@ -1,5 +1,7 @@
 #### Runs the markov chain model using the msm package ####
 
+### Data ###
+#Load data
 data <- read.csv("../../../Data/BTE/bte_sp_class.csv")[,-1]
 
-
+# Create dataframe with msm compatible format
